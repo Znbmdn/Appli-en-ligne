19f973bc263c0a11ee1d2ebbd77b3d6c2560070a
Diamande <ddelannay@etu.isara.fr>
2024-02-13 10:18:36 +0100

    mise en forme tableau

--- Interface_selection_recettes/app.R
+++ Interface_selection_recettes/app.R
@@ -1,8 +1,3 @@
-## Essai code interface sélection recettes en fonction de choix d'ingrédients et de plats
-
-# install.packages("shiny")
-# install.packages("DT")
-
 library(shiny)
 library(DT)
 
@@ -42,9 +37,9 @@ ui <- fluidPage(
     ),
     column(
       width = 3,
-      h3("Régimes alimentaires", align = "center", style = "color: #FFFFFF;"),
+      h3("Diète", align = "center", style = "color: #FFFFFF;"),
       selectizeInput("regime", "", choices = regimes, multiple = TRUE, options = list(
-        placeholder = "Sélectionnez les régimes alimentaires...",
+        placeholder = "Sélectionnez la diète...",
         plugins = list('remove_button')
       ))
     ),
@@ -59,7 +54,52 @@ ui <- fluidPage(
   ),
   fluidRow(
     DTOutput("recettes_table")
-  )
+  ),
+  tags$style(HTML("
+    .dataTables_wrapper {
+      background-color: #FFDAB9;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: none;
+    }
+  ")),
+  tags$style(HTML("
+    .modal-dialog {
+      max-width: 800px;
+    }
+    .modal-content {
+      background-color: #f8f9fa;
+    }
+    .modal-header {
+      border-bottom: none;
+    }
+    .modal-footer {
+      border-top: none;
+    }
+    .dataTables_filter {
+      text-align: right;
+    }
+    .dataTables_paginate {
+      float: right;
+    }
+    th, td {
+      border: 1px solid #ddd;
+      padding: 8px;
+    }
+    th {
+      padding-top: 12px;
+      padding-bottom: 12px;
+      text-align: left;
+      background-color: #FFA500;
+      color: white;
+    }
+    tr:nth-child(even) {
+      background-color: #FFE4B2;
+    }
+    tr:hover {
+      background-color: #FFD699;
+    }
+  "))
 )
 
 server <- function(input, output, session) {
@@ -112,12 +152,32 @@ server <- function(input, output, session) {
         fixedHeader = TRUE,
         language = list(
           search = "Recherche",
-          lengthMenu = "Montre _MENU_ entrées"
+          lengthMenu = "Montre _MENU_ recettes"
         )
+      ),
+      callback = JS("table.on('click', 'tr', function() {
+                      var data = table.row(this).data();
+                      Shiny.setInputValue('selected_recipe', data[0]);
+                      });")
+    )
+  })
+  
+  # Afficher une fenêtre détail recette lorsqu'une recette est cliquée
+  observeEvent(input$recettes_table_rows_selected, {
+    showModal(
+      modalDialog(
+        title = "Détails de la recette",
+        HTML(paste0("<h2>Recette : ", recettes_filtrées()$Nom[input$recettes_table_rows_selected], "</h2><p><strong>Type :</strong> ", recettes_filtrées()$Type[input$recettes_table_rows_selected], "</p><p><strong>Ingrédients :</strong> ", recettes_filtrées()$Ingrédients[input$recettes_table_rows_selected], "</p><p><strong>Instructions :</strong> ", recettes_filtrées()$Instructions[input$recettes_table_rows_selected], "</p><p><strong>Temps de préparation :</strong> ", recettes_filtrées()$Temps_de_preparation[input$recettes_table_rows_selected])),
+        footer = actionButton("close_modal_button", "Fermer", style = "background-color: #FFA500; color: white; border: none; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; border-radius: 5px;"),
+        easyClose = TRUE
       )
     )
+  })
+  
+  # Fermer la fenêtre détail recette
+  observeEvent(input$close_modal_button, {
+    removeModal()
   })
 }
 
 shinyApp(ui = ui, server = server)
-
